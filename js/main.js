e194ccfee2d8517f72080b04536aa98528a156da
angeloesaki <angeloesaki@gmail.com>
2020-07-05 18:28:44 +0900

    今月分の日付を取得

--- js/main.js
+++ js/main.js
@@ -2,4 +2,18 @@
 {
   const year = 2020;
   const month = 4; // 5月
+
+  //カレンダーは大きく分けて３つのパートに分かれている：先月、今月、来月
+  //以下は今月のパート
+  function getCalendarBody() {
+    const dates = [];
+    //末日は翌月１日の一日前という意味で、翌月０日目を指定することで取得することができる
+    const lastDate = new Date(year, month + 1, 0).getDate();
+
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push(i);
+    }
+    console.log(dates);
+  }
+  getCalendarBody();
 }
