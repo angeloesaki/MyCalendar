25a35d6f846818acc9c0545b47e73c6ccdca1a48
angeloesaki <angeloesaki@gmail.com>
2020-07-05 20:18:59 +0900

    前月分と翌月分の日付を取得

--- js/main.js
+++ js/main.js
@@ -6,8 +6,31 @@ console.clear();
   const year = 2020;
   const month = 4; // 5月
 
+  //前月分の日付を取得
+  function getCalendarHead() {
+    const dates = [];
+    //前月の末尾
+    const d = new Date(year, month, 0).getDate();
+    //カレンダーに表示する前月の日数
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      //30
+      //29, 30
+      //28, 29, 30
+      //配列の先頭に要素を追加するメソッド=unshift()
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
   //カレンダーは大きく分けて３つのパートに分かれている：先月、今月、来月
-  //以下は今月のパート
+  //今月分の日付を取得
   function getCalendarBody() {
     const dates = [];
     //末日は翌月１日の一日前という意味で、翌月０日目を指定することで取得することができる
@@ -22,5 +45,23 @@ console.clear();
     }
     console.log(dates);
   }
-  getCalendarBody();
+
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
+  // getCalendarHead();
+  // getCalendarBody();
+  getCalendarTail();
 }
